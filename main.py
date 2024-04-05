c06c4cfb7dc988cd979ec90665be325a7f7c76f9
Andrew <ak.andrewkang@gmail.com>
2024-04-04 22:03:42 -0400

    Split data into test and training sets
    - Adapted shuffle_and_split_data() from "Hands-On Machine Learning" by Aurelian Geron
    - Used shuffle_and_split_data() to split data into training and test sets

--- main.py
+++ main.py
@@ -18,3 +18,12 @@ column_headers = ([
 
 data = pd.read_csv("data/UFRGS_exam_gpa.csv", header = 0, names = column_headers)
 
+# Adapted from "Hands-On Machine Learning" by Aurelian Geron (Ch. 2)
+def shuffle_and_split_data(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = shuffle_and_split_data(data, 0.2)
